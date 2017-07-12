308a4bf120aabe80cf2c2091bacc6e696fd565d9
Francois Zaninotto <fzaninotto@gmail.com>
2017-07-12 11:09:18 +0200

    Remove not working libs to make the package lighter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as createApolloClient } from './createApolloClient';
-export { default as graphQLClientServer } from './graphQLClientServer';
+//export { default as createApolloClient } from './createApolloClient';
+//export { default as graphQLClientServer } from './graphQLClientServer';
 export { default as jsonGraphqlExpress } from './jsonGraphqlExpress';
