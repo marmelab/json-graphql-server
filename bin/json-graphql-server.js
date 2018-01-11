0258f61fc08f5d3d5ae2e0a14a242d2bd2938399
gildas <gildas.garcia@gmail.com>
2018-01-11 17:37:38 +0100

    Review: var for older node versions

--- bin/json-graphql-server.js
+++ bin/json-graphql-server.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 require('reify');
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const JsonGraphqlServer = require('../lib/json-graphql-server.node.min').default;
+var path = require('path');
+var express = require('express');
+var cors = require('cors');
+var JsonGraphqlServer = require('../lib/json-graphql-server.node.min').default;
 
-const dataFilePath = process.argv.length > 2 ? process.argv[2] : './data.json';
-const data = require(path.join(process.cwd(), dataFilePath));
-const PORT = 3000;
-const app = express();
+var dataFilePath = process.argv.length > 2 ? process.argv[2] : './data.json';
+var data = require(path.join(process.cwd(), dataFilePath));
+var PORT = 3000;
+var app = express();
 
 app.use(cors());
 app.use('/', JsonGraphqlServer(data));
 app.listen(PORT);
-const msg = `GraphQL server running with your data at http://localhost:${PORT}/`;
+var msg = `GraphQL server running with your data at http://localhost:${PORT}/`;
 console.log(msg); // eslint-disable-line no-console
 
 process.on('unhandledRejection', (reason, p) => {
