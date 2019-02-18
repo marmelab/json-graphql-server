8a76ff71954da08ab4c2b8495a53e8f687ce5e2c
Orta Therox <orta.therox@gmail.com>
2019-02-18 07:00:28 -0500

    Use the right values for the random indexes

--- src/resolver/Query/random.js
+++ src/resolver/Query/random.js
@@ -5,13 +5,13 @@ const getRandomNumber = (min, max) =>
 
 export default (entityData = []) => (_, { count = 3, filter = {} }) => {
     let items = [...entityData];
-
     items = applyFilters(items, filter);
+
     // cut down to count items
     let returnCount = Math.min(count, items.length);
     let randomItems = [];
     for (let i = 0; i < returnCount; i++) {
-        randomItems.push(items[getRandomNumber(0, returnCount - 1)]);
+        randomItems.push(items[getRandomNumber(0, items.length - 1)]);
     }
 
     return randomItems;
