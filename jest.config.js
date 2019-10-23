3b56c12b818e5a7a18696b9d624ed4c0028fe246
Alexis Janvier <contact@alexisjanvier.net>
2019-10-23 12:36:38 +0200

    configuration dotfiles cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  verbose: true,
-  projects: [
-    {
-      displayName: "graphql-schema-from-json",
-      testMatch: ["<rootDir>/packages/graphql-schema-from-json/**/*.spec.js"]
-    },
-    {
-      displayName: "json-graphql-server",
-      testMatch: ["<rootDir>/packages/json-graphql-server/**/*.spec.js"]
-    }
-  ]
+    verbose: true,
+    projects: [
+        {
+            displayName: 'graphql-schema-from-json',
+            testMatch: [
+                '<rootDir>/packages/graphql-schema-from-json/**/*.spec.js',
+            ],
+        },
+        {
+            displayName: 'json-graphql-server',
+            testMatch: ['<rootDir>/packages/json-graphql-server/**/*.spec.js'],
+        },
+    ],
 };
