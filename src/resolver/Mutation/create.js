8c4266bec971354061cea5801279c2c3d624dad4
underscore05 <roquerichardneil@gmail.com>
2021-03-31 18:22:43 +0800

    Create create.js
    Revert back the initial id to 0

--- src/resolver/Mutation/create.js
+++ src/resolver/Mutation/create.js
@@ -1,6 +1,6 @@
 export default (entityData = []) => (_, entity) => {
     const newId =
-        entityData.length > 0 ? entityData[entityData.length - 1].id + 1 : 1;
+        entityData.length > 0 ? entityData[entityData.length - 1].id + 1 : 0;
     const newEntity = Object.assign(entity, { id: newId });
     entityData.push(newEntity);
     return newEntity;
